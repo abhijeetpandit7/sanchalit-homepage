f871450e5912291fab875c742ef1d1c376b839f8
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2024-12-14 12:16:18 +0000

    Replace react-router-dom with react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router";
 import { Navbar } from "./components/";
 
 const Account = lazy(() => import("./pages/Account/Account"));
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router";
 import { brandLogo } from "../../utils";
 
 export const Navbar = () => {
